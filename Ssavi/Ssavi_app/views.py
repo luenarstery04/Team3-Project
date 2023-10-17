98bc6765857aee38305408afae2fd410178f3bba
LuenArstery04 <csh98.dev@gmail.com>
2023-10-17 15:37:14 +0900

    spotipy 키 추가

--- Ssavi/Ssavi_app/views.py
+++ Ssavi/Ssavi_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
+import spotipy
+import spotipy_secret
+from spotipy.oauth2 import SpotifyClientCredentials
 
+client_credentials_manager = SpotifyClientCredentials(client_id=spotipy_secret.client_id, client_secret=spotipy_secret.client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # Create your views here.
 def index(request):
@@ -10,3 +15,4 @@ def detail(request):
 
 def recommend(request):
     return render(request, 'Ssavi_app/recommend.html')
+
