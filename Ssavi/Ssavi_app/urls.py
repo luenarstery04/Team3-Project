e88cfd562c93ab0203287fb52e63c8651ae1647e
임시 <a@a.com>
2023-10-20 09:47:08 +0900

    ltj

--- Ssavi/Ssavi_app/urls.py
+++ Ssavi/Ssavi_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.album_list, name='index'),
     path('detail/', views.detail, name='detail'),
+    path('recommend/', views.recommend, name='recommend'),
+    path('album_list/', views.album_list, name='album_list'),
 ]
--- Ssavi/Ssavi_app/views.py
+++ Ssavi/Ssavi_app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import Albums
+from django.template.loader import render_to_string  
+from django.http import JsonResponse
+from django.template import loader
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
@@ -6,3 +11,14 @@ def index(request):
 
 def detail(request):
     return render(request, 'Ssavi_app/detail.html')
+
+def recommend(request):
+    return render(request, 'Ssavi_app/recommend.html')
+
+def album_list(request):
+    page = int(request.GET.get('page', 1))
+    albums_per_page = 15 # 페이지당 앨범 수
+    albums = Albums.objects.all()
+    paginator = Paginator(albums, albums_per_page)
+    page_albums = paginator.get_page(page)
+    return render(request, 'Ssavi_app/index.html', {'albums': page_albums})
