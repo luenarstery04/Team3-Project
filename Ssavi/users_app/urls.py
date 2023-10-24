41968caa7bbb1e2f5cd7e4cc29c6000aa6a0b305
LuenArstery04 <csh98.dev@gmail.com>
2023-10-24 11:07:05 +0900

    회원정보 수정 기능 구현 완료

--- Ssavi/users_app/urls.py
+++ Ssavi/users_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('sign_up/', views.sign_up, name='sign_up'),
     path('sign_out/', views.sign_out, name='sign_out'),
     path('id_check/', views.id_check, name='id_check'),
+    path('user_update/', views.user_update, name='user_update'),
+    path('user_pwd_check/', views.password_check, name='pwd_check')
 ]
