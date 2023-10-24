f48a2c740db40956c1215c3500975def7cacd0bb
임시 <a@a.com>
2023-10-24 17:09:22 +0900

    ltj

--- Ssavi/Ssavi_app/urls.py
+++ Ssavi/Ssavi_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     
     path('music_recommend/', views.music_recommend, name='music_recommend'),
     path('like_track/', views.like_track, name='like_track'),
-
+    path('like_album/', views.like_album, name='like_album'),
 ]
--- Ssavi/Ssavi_app/views.py
+++ Ssavi/Ssavi_app/views.py
@@ -22,7 +22,21 @@ def album_list(request):
     albums = Albums.objects.all()
     paginator = Paginator(albums, albums_per_page)
     page_albums = paginator.get_page(page)
-    return render(request, 'Ssavi_app/index.html', {'albums': page_albums})
+
+    likealbum = []
+
+    if request.user.is_authenticated:
+        user_id = request.user.id
+        for i in albums:
+            if LikedAlbum.objects.filter(id=user_id, album_id=i.album_id).exists():
+                likealbum.append(i.album_id)
+
+    context = {
+        'albums': page_albums,
+        'likealbum' : likealbum
+    }
+
+    return render(request, 'Ssavi_app/index.html', context)
 
 
 def music_recommend(request):
@@ -123,3 +137,25 @@ def like_track(request):
         except Tracks.DoesNotExist:
             return JsonResponse({'error': '트랙이 존재하지 않습니다.'}, status=404)
         
+def like_album(request):
+    if request.method == 'POST':
+        album_id = request.POST.get('album_id')
+        id = request.POST.get('id')
+      
+        try:
+            id = UsersAppUser.objects.get(id=id)
+            album_id = Albums.objects.get(album_id=album_id)
+
+            liked_album, created = LikedAlbum.objects.get_or_create(album=album_id, id=id)
+
+            if created:
+                message = "앨범 좋아요"
+            else:
+                liked_album.delete()
+                message = "앨범 좋아요 해제"
+
+            return JsonResponse({'message': message})
+        except UsersAppUser.DoesNotExist:
+            return JsonResponse({'error': '사용자가 존재하지 않습니다.'}, status=404)
+        except Albums.DoesNotExist:
+            return JsonResponse({'error': '트랙이 존재하지 않습니다.'}, status=404)
